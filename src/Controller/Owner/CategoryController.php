660a75b9afa27852d5c80ff30b56e391ccd79302
MouhamedDridii <mouhamed.eldridi@gmail.com>
2025-08-05 16:17:11 +0100

    test commit

--- src/Controller/Owner/CategoryController.php
+++ src/Controller/Owner/CategoryController.php
@@ -11,6 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+//
 
 #[Route('owner/category', name: 'owner.category.')]
 
